c66f6e11befbc8cd58242c2cf8a0cc242563c613
Justin <justinshi0714@gmail.com>
2022-06-11 21:14:42 -0400

    basic chat UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+
+import TextField from '@mui/material/TextField';
+import { Box } from '@mui/system';
+import { Button, Container, Grid } from '@mui/material';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Chat App
       </header>
+
+      <Grid container style = {{bottom:20, position: "fixed"}} spacing = {4}>
+        <Grid item xs={9} style = {{marginLeft:40}} pl={0} pt={0}>
+          <TextField id="chat box" width="90%" display="flex" fullWidth="true" size = "small"/>
+        </Grid>
+        <Grid item xs = {2} display="flex">
+          <Button variant="contained">Send</Button>
+        </Grid>
+        
+      </Grid>
+      
     </div>
   );
 }
