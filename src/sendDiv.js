2be337d372f886ca386bcb92a7690f5063d6d351
Justin <justinshi0714@gmail.com>
2022-06-11 22:53:12 -0400

    add onclick to button, change true from string to bool

--- src/sendDiv.js
+++ src/sendDiv.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TextField from '@mui/material/TextField';
 import { Button, Grid } from '@mui/material';
 const SendDiv = () => {
+  const [value, setValue] = useState("");
+  const sendMessage = () => {
+    console.log(value);
+  }
   return (
     <Grid container style = {{bottom:20, position: "fixed"}} spacing = {4}>
     <Grid item xs={9} style = {{marginLeft:40}} pl={0} pt={0}>
-      <TextField id="chat box" width="90%" display="flex" fullWidth="true" size = "small" maxRows={6} multiline="true"/>
+      <TextField 
+        id="chat box" 
+        width="90%" 
+        display="flex" 
+        fullWidth={true} 
+        size = "small" 
+        maxRows={6} 
+        multiline={true}
+        value={value}
+        onChange ={ (v) => setValue(v.target.value)}
+        />
     </Grid>
     <Grid item xs = {2} display="flex">
-      <Button variant="contained">Send</Button>
+      <Button variant="contained" onClick={sendMessage}>Send</Button>
     </Grid>
     
   </Grid>
