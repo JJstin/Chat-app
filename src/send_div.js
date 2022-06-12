0dd459ee76fefe95f3b930cfc93b4ef2497e7ae0
Justin <justinshi0714@gmail.com>
2022-06-11 22:05:40 -0400

    make message filed multi line

--- src/send_div.js
+++ src/send_div.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import TextField from '@mui/material/TextField';
-import { Box } from '@mui/system';
-import { Button, Container, Grid } from '@mui/material';
+import { Button, Grid } from '@mui/material';
 const send_div = () => {
   return (
     <Grid container style = {{bottom:20, position: "fixed"}} spacing = {4}>
     <Grid item xs={9} style = {{marginLeft:40}} pl={0} pt={0}>
-      <TextField id="chat box" width="90%" display="flex" fullWidth="true" size = "small"/>
+      <TextField id="chat box" width="90%" display="flex" fullWidth="true" size = "small" maxRows={6} multiline="true"/>
     </Grid>
     <Grid item xs = {2} display="flex">
       <Button variant="contained">Send</Button>
