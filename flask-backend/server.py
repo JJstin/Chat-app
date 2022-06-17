5fcf2817797e2051050546ed29b63818d956615e
xiwa1866 <xiwa1866@gmail.com>
2022-06-16 23:43:44 -0400

    add db connection

--- flask-backend/server.py
+++ flask-backend/server.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from pymongo import MongoClient
+def get_database():
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = "mongodb+srv://justinshi:3fW61ONLirpGInoT@Cluster0.mongodb.net/test"
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    from pymongo import MongoClient
+    client = MongoClient(CONNECTION_STRING)
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client['users']
+    
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":    
+    
+    # Get the database
+    dbname = get_database()
+
 
 app = Flask(__name__)
-
 # login API Route
 
 @app.route("/login")
