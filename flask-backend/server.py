95f389c395235f84393dd693841126be5ccbce76
xiwa1866 <xiwa1866@gmail.com>
2022-06-18 10:51:54 +0800

    add check_login

--- flask-backend/server.py
+++ flask-backend/server.py
@@ -1,15 +1,18 @@
 from flask import Flask
+from datetime import datetime
+import pymongo
 from pymongo import MongoClient
+
 def get_database():
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb+srv://justinshi:3fW61ONLirpGInoT@Cluster0.mongodb.net/test"
+    CONNECTION_STRING = "mongodb+srv://justinshi:3fW61ONLirpGInoT@cluster0.ajzw97t.mongodb.net/?retryWrites=true&w=majority"
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     from pymongo import MongoClient
     client = MongoClient(CONNECTION_STRING)
 
     # Create the database for our example (we will use the same database throughout the tutorial
-    return client['users']
+    return client['test']
     
 # This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":    
@@ -25,5 +28,9 @@ app = Flask(__name__)
 def login():
     return {"login info": ["username: 3", "password: 4"]}
 
+@app.route("/check_login")
+def check_login():
+    dbname['login_times'].insert_one({"time": datetime.now()})
+    return {"time": datetime.now()}
 if __name__ == "__main__":
     app.run(debug=True)
