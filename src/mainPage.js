1717ba25607d3110b2da9cbfaead645e8f8b3314
xiwa1866 <xiwa1866@gmail.com>
2022-06-19 17:11:28 -0400

    add navigation on main page

--- src/mainPage.js
+++ src/mainPage.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import { Button }from '@mui/material';
-
+import React from "react";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const MainPage = () => {
-
+  let navigate = useNavigate();
+  
   return (
-    <div>Welcome to Chat App!
-        <Button>Log in</Button>
-        <Button> Sign up</Button>
+    <div>
+      Welcome to Chat App!
+      <Button
+        onClick={() => {
+          navigate("/login");
+        }}
+      >
+        Log in
+      </Button>
+      <Button onClick={() => {
+          navigate("/signup");
+        }}> Sign up</Button>
     </div>
-    
-    
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
